c97b4bfd900a315db4ef8273c669d43fd6268bad
khan blise <144198328+Osasblaisekhan@users.noreply.github.com>
2025-06-04 05:43:43 +0100

    learnning_app

---
+++ srcipt.js
@@ -0,0 +1,8 @@
+const mobileMenu = document.getElementById('mobile-menu');
+mobileMenu.addEventListener('click',()=>{
+    const navBar = document.getElementById('nav-bar');
+    navBar.classList.toggle('active');
+});
+
+
+
