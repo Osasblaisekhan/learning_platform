eb305b0cb6f80953d783d8eea97e1a58b0150455
khan blise <144198328+Osasblaisekhan@users.noreply.github.com>
2025-06-05 09:11:53 +0100

    added_package.json

--- srcipt.js
+++ srcipt.js
@@ -6,3 +6,33 @@ mobileMenu.addEventListener('click',()=>{
 
 
 
+const list = [1,2,3,4,5,6,-1,-3,-19,-100,-67,-54,76, -12,87,-65,-90];
+
+
+
+const random = () =>{
+    const n1 = (Math.floor(Math.random()*9)+""+
+Math.floor(Math.random()*9)+""+Math.floor(Math.random()*9)+""+Math.floor(Math.random()*9));
+
+
+console.log(n1);
+    
+};
+
+random();
+
+
+list.map((num)=>{
+    if(num<0){
+        let yo = num*-1
+        console.log('neg', yo)
+    }else{
+        let yoo = num-5;
+        console.log('sub', yoo)
+    }
+})
+console.log(list);
+
+
+
+
